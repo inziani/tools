eabec13f52a25991b093d80f1fb834a369005c38
“inziani” <ValentineRobai.Inziani@gmail.com>
2021-01-18 12:19:11 +0300

    project set up

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function printMessage(msg) {
+    console.log(`Message: ${msg}`);
+}
+printMessage('Hello TypeScript');
+printMessage(100);
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function printMessage(msg: string){
+  console.log(`Message: ${msg}`);
+}
+printMessage('Hello TypeScript');
+printMessage(`100`);
